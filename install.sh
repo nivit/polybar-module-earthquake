fc25b30f378c3d545cc58613e81915286e345ba3
Nicola Stanislao Vitale <nivit@rocketmail.com>
2018-12-31 21:11:25 +0100

    Add install.sh script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# this script installs the polybar module earthquake
+
+destdir=${HOME}/.config/polybar/scripts/earthquake
+polybar_conf=${HOME}/.config/polybar/config
+
+install -d ${destdir}
+install -m 554 earthquake.sh ${destdir}
+install -b -m 644 *.conf ${destdir}
+
+if [ -f ${polybar_conf} ]; then
+    cat polybar.conf >> ${polybar_conf}
+else
+    echo "Add the following lines to your polybar configuration:"
+    cat polybar.conf
+fi
