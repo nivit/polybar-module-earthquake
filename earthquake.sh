125a25e5e7a0fed3746e8aa4dd8699dbf8815182
Nicola Stanislao Vitale <nivit@rocketmail.com>
2019-01-03 21:27:54 +0100

    Fix ShellCheck warnings in Bash scripts

--- earthquake.sh
+++ earthquake.sh
@@ -72,13 +72,14 @@ last_download=${module_obj_dir}/last_download_time
 google_maps_url='https://maps.google.com/maps/@'
 usgs_url='https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/1.0_hour.geojson'
 
-if [ ! -f ${module_obj_dir} ]; then
-    mkdir -p ${module_obj_dir}
+if [ ! -f "${module_obj_dir}" ]; then
+    mkdir -p "${module_obj_dir}"
 fi
 
 # override default values
-if [ -f ${earthquake_conf} ]; then
-    . ${earthquake_conf}
+if [ -f "${earthquake_conf}" ]; then
+    # shellcheck source=news.conf disable=SC1091
+    . "${earthquake_conf}"
 fi
 
 # check availability of the requested programs
@@ -110,51 +111,53 @@ if ! xdg_cmd_loc="$(type -p "${xdg_cmd}")" || \
     exit 0
 fi
 
-if [ -f ${last_download} ]; then
-    old_time=$(cat ${last_download})
+if [ -f "${last_download}" ]; then
+    old_time=$(cat "${last_download}")
     new_time=$(date +%s)
 
-    delta_time=$((${new_time} - ${old_time}))
+    delta_time=$((new_time - old_time))
 
     if [ ${delta_time} -gt ${download_interval} ]; then
-        rm -f ${last_download}
+        rm -f "${last_download}"
     fi
 fi
 
 # download USGS data
-if [ ! -f ${last_download} ]; then
-    if [ "${debug}" = "1" -a -f ${earthquakes_json}.test ]; then
-        cp ${earthquakes_json}.test ${earthquakes_json}
+if [ ! -f "${last_download}" ]; then
+    if [ "${debug}" = "1"  ] &&
+       [ -f "${earthquakes_json}".test ]; then
+        cp "${earthquakes_json}".test "${earthquakes_json}"
     else
-        ${fetch_cmd} -o ${earthquakes_json} ${fetch_options} ${usgs_url}
+        ${fetch_cmd} -o "${earthquakes_json}" "${fetch_options}" "${usgs_url}"
     fi
 
     # extract earthquakes ids
-    jq -r -M '.features[]|.id' ${earthquakes_json} > ${earthquakes_ids}
-    cp ${earthquakes_ids} ${earthquakes_ids}.all
+    jq -r -M '.features[]|.id' "${earthquakes_json}" > "${earthquakes_ids}"
+    cp "${earthquakes_ids}" "${earthquakes_ids}".all
 
-    date +%s > ${last_download}
+    date +%s > "${last_download}"
 fi
 
 # check whether there are earthquakes in the last hour
-if [ -f ${earthquakes_json} ]; then
-    no_data=$(jq -r -M 'if .metadata.count > 0 then 1 else 0 end' ${earthquakes_json})
+if [ -f "${earthquakes_json}" ]; then
+    no_data=$(jq -r -M 'if .metadata.count > 0 then 1 else 0 end' "${earthquakes_json}")
 
-    if [ ! -s ${earthquakes_json} -o "${no_data}" = "0" ]; then
+    if [ ! -s "${earthquakes_json}" ] || \
+       [ "${no_data}" = "0" ]; then
         echo '-- no earthquake data --'
         exit 0
     fi
 fi
 
 if [ -n "$1" ]; then
-    current_id=$(cat ${current_earthquake})
+    current_id=$(cat "${current_earthquake}")
 else
-    current_id=$(head -n 1 ${earthquakes_ids} | tee ${current_earthquake})
-    sed -i.bak -e '1d' ${earthquakes_ids}
+    current_id=$(head -n 1 "${earthquakes_ids}" | tee "${current_earthquake}")
+    sed -i.bak -e '1d' "${earthquakes_ids}"
 
-    if [ ! -s ${earthquakes_ids} \
-        -a -f ${earthquakes_ids}.all ]; then
-        cp -f ${earthquakes_ids}.all ${earthquakes_ids}
+    if [ ! -s "${earthquakes_ids}" ] && \
+       [ -f "${earthquakes_ids}".all ]; then
+        cp -f "${earthquakes_ids}".all "${earthquakes_ids}"
     fi
 fi
 
@@ -162,6 +165,7 @@ fi
 case "${earthquake_mode}" in
     "all")
         jq_args="-r -M --arg id ${current_id}"
+        # shellcheck disable=SC2016
         jq_selector='.features[]|select(.id==$id?)|'
         ;;
     "latest")
@@ -178,8 +182,9 @@ esac
 if [ -n "$1" ]; then
     case "$1" in
         "open-google-map")
+            # shellcheck disable=SC2086
             coords=$(jq ${jq_args} ${jq_selector}'[.geometry.coordinates[1,0]?] | tostring | ltrimstr("[") | rtrimstr("]")' \
-                ${earthquakes_json})
+                "${earthquakes_json}")
 
             if [ "${satellite_view}" = "yes" ]; then
                 url="${google_maps_url}${coords},${zoom_factor}z/data=!3m1!1e3"
@@ -187,13 +192,14 @@ if [ -n "$1" ]; then
                 url="${google_maps_url}${coords},${zoom_factor}z"
             fi
 
-            ${xdg_cmd} ${url}&
+            ${xdg_cmd} "${url}"&
 
             exit 0
         ;;
         "open-event-page")
+            # shellcheck disable=SC2086
             url=$(jq ${jq_args} ${jq_selector}.properties.url ${earthquakes_json})
-            ${xdg_cmd} ${url}&
+            ${xdg_cmd} "${url}"&
             exit 0
         ;;
         "toggle-earthquake-mode")
@@ -205,8 +211,8 @@ if [ -n "$1" ]; then
                 earthquake_mode=latest
             fi
 
-            sed -i.bak -e "s/^\(earthquake_mode=\).*/\1${earthquake_mode}/" \
-                ${earthquake_conf}
+            sed -i.bak -e "s/^\\(earthquake_mode=\\).*/\\1${earthquake_mode}/" \
+                "${earthquake_conf}"
         ;;
         *)
             echo "-- unknown argument --"
@@ -215,6 +221,7 @@ if [ -n "$1" ]; then
     esac
 else
     if [ "${tsunami_alert}" = "yes" ]; then
+        # shellcheck disable=SC2086
         tsunami=$(jq ${jq_args} ${jq_selector}.properties.tsunami ${earthquakes_json})
         if [ "${tsunami}" = "1" ]; then
             tsunami_msg=" %{B#f00 F#fff}-- ${tsunami_icon} TSUNAMI ALERT --"
@@ -224,6 +231,7 @@ else
     fi
 
     if [ "X${underline_title}X" = "XyesX" ]; then
+        # shellcheck disable=SC2086
         mag=$(jq ${jq_args} ${jq_selector}.properties.mag ${earthquakes_json})
 
         case "${mag%%.*}" in
@@ -248,6 +256,7 @@ else
         underline_format=""
     fi
 
+    # shellcheck disable=SC2086
     title=$(jq ${jq_args} ${jq_selector}.properties.title ${earthquakes_json})
 
     if [ "X${show_icon}X" = "XyesX" ]; then
@@ -257,12 +266,13 @@ else
     fi
 
     if [ "X${show_time}X" = "XyesX" ]; then
-        time=" - $(jq ${jq_args} ${jq_selector}'(.properties.time?/1000|todate)' ${earthquakes_json})"
+        # shellcheck disable=SC2086
+        time=" - $(jq ${jq_args} ${jq_selector}'(.properties.time?/1000|todate)' "${earthquakes_json}")"
     else
         time=""
     fi
 
-    echo ${underline_format}${icon}${title}${tsunami_msg}${time}
+    echo "${underline_format}${icon}${title}${tsunami_msg}${time}"
 
     exit 0
 fi
--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@
 destdir=${HOME}/.config/polybar/scripts/earthquake
 polybar_conf=${HOME}/.config/polybar/config
 
-install -d ${destdir}
-install -b -m 644 *.conf ${destdir}
-install -m 554 earthquake.sh ${destdir}
+install -d "${destdir}"
+install -b -m 644 ./*.conf "${destdir}"
+install -m 554 earthquake.sh "${destdir}"
 
-if [ -f ${polybar_conf} ]; then
-    cat polybar.conf >> ${polybar_conf}
+if [ -f "${polybar_conf}" ]; then
+    cat polybar.conf >> "${polybar_conf}"
 else
     echo "Add the following lines to your polybar configuration:"
     cat polybar.conf
